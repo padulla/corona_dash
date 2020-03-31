39bd45d0d807af8c7244cf9d165798b5d12bbb33
padulla <leandro.padulla@gmail.com>
2020-03-30 22:23:37 -0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 # corona_dash
-Dashboard da evolução dos casos do Covid-19 usando a base de dados do Johns Hopkins CSSE
---
+++ get_data.R
@@ -0,0 +1,11 @@
+
+
+library (RCurl)
+
+
+
+
+download <- getURL("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv")
+data <- read.csv (text = download)
+data
+
