7920c8adbd54fbf5242fe8e1e9ebe687500cc887
padulla <leandro.padulla@gmail.com>
2020-03-31 10:51:08 -0300

    Update get_data.R

--- get_data.R
+++ get_data.R
@@ -7,5 +7,5 @@ library (RCurl)
 
 download <- getURL("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv")
 data <- read.csv (text = download)
-data
+teste <-tail(data,5)
 
