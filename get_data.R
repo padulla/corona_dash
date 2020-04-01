545f7bc431d8f71650a417589b91a7184d8b759c
padulla <leandro.padulla@gmail.com>
2020-03-31 22:40:42 -0300

    Update get_data.R

--- get_data.R
+++ get_data.R
@@ -1,11 +1,26 @@
 
 
 library (RCurl)
+library('magrittr')
 
 
 
+download <- RCurl::getURL("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv")
 
-download <- getURL("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv")
-data <- read.csv (text = download)
-teste <-tail(data,5)
+#dim(data) = 254 73
+data <- read.csv(text = download, 
+                 stringsAsFactors = FALSE, 
+                 colClasses = c(rep('character', 4), rep('numeric', 73-4)))
 
+dates <- colnames(data)[-c(1:4)] %>% 
+  strsplit(., 'X') %>% 
+  lapply(., '[', 2) %>% 
+  unlist() %>% 
+  as.Date(format = "%m.%d.%y")
+
+
+DataColNames <- colnames(data)[1:4]
+colnames(data) <- c(DataColNames, format(dates, '%m/%d/%y'))
+
+ncol(data)
+da
