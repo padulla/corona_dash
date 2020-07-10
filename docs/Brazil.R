a70a0f0e0dcf468ef357e17d78fdc50d33ab77cc
padulla <leandro.padulla@gmail.com>
2020-07-10 08:09:02 -0300

    09/07

--- docs/Brazil.R
+++ docs/Brazil.R
@@ -273,7 +273,7 @@ BrDeathsLb <-BrDeaths[-c(28, 29), ]
 N = ncol(RawBrPop)
 BrPop <- RawBrPop %>% 
            rename_all(~ letters[1:N]) %>% 
-           select(c, k) %>% 
+           select(a, i) %>% 
            rename_all(~ c("Estado", "Pop")) %>% 
            arrange(Estado)
 
